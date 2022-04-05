6323135f47f773ca3eb74e8da09581c90e5aa1da
Diken Maharjan <dikenmaharjan10@gmail.com>
2022-04-05 18:33:35 +0545

    create function to fetch popular articles

--- app/src/main/java/diken/nyarticles/network/apiservices/ArticleApi.kt
+++ app/src/main/java/diken/nyarticles/network/apiservices/ArticleApi.kt
@@ -1,5 +1,19 @@
 package diken.nyarticles.network.apiservices
 
+import diken.nyarticles.network.response.viewedarticleresponse.ViewedArticleResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface ArticleApi {
-    
+
+    @GET("svc/mostpopular/v2/viewed/0.json")
+    suspend fun getPopularArticle(
+        @Query("api-key") apiKey: String = API_KEY
+    ): Response<ViewedArticleResponse>
+
+    companion object {
+        private const val API_KEY = "AEBRAABki2KAlk7jWgYIg7wyZzvp8WjD"
+    }
+
 }
