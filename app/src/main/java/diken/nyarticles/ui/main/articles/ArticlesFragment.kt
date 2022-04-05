4cedb9b97844493720ddf40588fc5fbed4e580c1
Diken Maharjan <dikenmaharjan10@gmail.com>
2022-04-05 09:06:49 +0545

    set up recycler view in articles fragment

--- app/src/main/java/diken/nyarticles/ui/main/articles/ArticlesFragment.kt
+++ app/src/main/java/diken/nyarticles/ui/main/articles/ArticlesFragment.kt
@@ -6,8 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import diken.nyarticles.databinding.FragmentArticlesBinding
+import diken.nyarticles.ui.main.articles.recyclerview.ArticlesRVA
 
 
 @AndroidEntryPoint
@@ -16,13 +18,23 @@ class ArticlesFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val viewModel: ArticlesFragmentViewModel by viewModels()
+    private val adapter = ArticlesRVA()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentArticlesBinding.inflate(layoutInflater)
+        setUpRecyclerView()
         return binding.root
+    }
+
+    private fun setUpRecyclerView() {
+        binding.articlesFrgArticlesRV.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            this.adapter = adapter
+        }
     }
 
     override fun onDestroyView() {
