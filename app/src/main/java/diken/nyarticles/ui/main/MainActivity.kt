6c1a7ca26dae31069b48f4f175b5050b09e5b614
Diken Maharjan <dikenmaharjan10@gmail.com>
2022-04-05 08:35:01 +0545

    set up NavController in MainActivity

---
+++ app/src/main/java/diken/nyarticles/ui/main/MainActivity.kt
@@ -0,0 +1,24 @@
+package diken.nyarticles.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import diken.nyarticles.R
+import diken.nyarticles.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        navController = findNavController(R.id.mainAct_navHostFragmentContainer)
+        setupActionBarWithNavController(navController)
+    }
+}
